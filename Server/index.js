26afd8e59834d04f5f709688211f682060c7a20b
ItaySN <itayfic@gmail.com>
2020-08-20 17:35:14 +0300

    Createe functions to post and get the records of the app
    I used fs and json

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const app = express();
+
 const fs = require("fs").promises;
 
 
 app.use(express.json());
 
-let records = [];
 
 
-app.post('/records', (req, res) =>{
-    records.push(req.body);
-    res.send(req.body);
+
+app.post('/api/v1/post', async (req, res) =>{
+    let data = req.body;
+    let file = await fs.readFile('records.json');
+    file = JSON.parse(file);
+    file.push(data);
+    fs.writeFile('records.json',JSON.stringify(file));
+    res.send("success");
 });
 
-app.get('/records', (req, res) => {
-    res.send(products);
+app.get('/api/v1/records', async (req, res) => {
+    let data = await fs.readFile('records.json');
+    data = JSON.parse(data);
+    res.send(data);
 });
 
 app.listen(8080);
